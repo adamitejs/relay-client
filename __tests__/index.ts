4ff2b2f0b5aadcffaa16c332512ba06c608ef747
Jesse Dunlap <jesse@hydrais.com>
2019-05-18 18:06:39 -0500

    Fixed unit tests

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -4,10 +4,9 @@ jest.mock("../src/RelayClient");
 
 describe("index", () => {
   it("should return an RelayClient instance", () => {
-    const mockApp = { app: "1234" };
-    const mockConfig = { abc: "123" };
-    const server = index(mockApp, mockConfig);
-    expect(RelayClient).toHaveBeenCalledWith(mockApp, mockConfig);
+    const mockConfig = { url: "", service: "", apiKey: "" };
+    const server = index(mockConfig);
+    expect(RelayClient).toHaveBeenCalledWith(mockConfig);
     expect(server).toBe((RelayClient as any).mock.instances[0]);
   });
 });
